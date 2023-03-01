8ac4fe63adc5616d5d904e8cc9c79febcd139dd1
Евгений Воронцов <sejeenn@gmail.com>
2023-03-01 22:39:55 +0300

    Tags and filters 01.03.23

---
+++ mysite/shopapp/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+from timeit import default_timer
+
+
+# Create your views here.
+def shop_index(request: HttpRequest):
+    products = [
+        ('Laptop', 1999),
+        ('Desktop', 2999),
+        ('Smartphone', 999),
+    ]
+    context = {
+        "time_running": default_timer(),
+        "products": products,
+    }
+    return render(request, 'shopapp/shop-index.html', context=context)
