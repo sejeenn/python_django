a0ed7b4abf8b7265d856daffa2d09660b13dbc3e
Евгений Воронцов <sejeenn@gmail.com>
2023-03-02 12:13:08 +0300

    Tags and filters 02.03.23

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -5,6 +5,13 @@ from timeit import default_timer
 
 # Create your views here.
 def shop_index(request: HttpRequest):
+    colors = {
+        'red': '#FF0000',
+        'green': '#008000',
+        'yellow': '#FF0000',
+        'blue': '#0000FF',
+        'gray': '#C0C0C0',
+    }
     products = [
         ('Laptop', 1999),
         ('Desktop', 2999),
@@ -13,5 +20,6 @@ def shop_index(request: HttpRequest):
     context = {
         "time_running": default_timer(),
         "products": products,
+        "colors": colors,
     }
     return render(request, 'shopapp/shop-index.html', context=context)
