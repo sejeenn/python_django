22f1a2add6d299152e2760697fbc9cc05e6f61f1
Eugene Vorontsov <sejeenn@gmail.com>
2023-03-07 12:36:44 +0300

    07.03.23_M_03_Done

---
+++ M_03_DatabaseAndModels/mysite/shopapp/views.py
@@ -0,0 +1,40 @@
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+from timeit import default_timer
+from django.contrib.auth.models import Group
+from .models import Product, Order
+
+
+# Create your views here.
+def shop_index(request: HttpRequest):
+    products = [
+        ('Laptop', 1999),
+        ('Desktop', 2999),
+        ('Smartphone', 999),
+    ]
+    context = {
+        "time_running": default_timer(),
+        "products": products,
+    }
+    return render(request, 'shopapp/shop-index.html', context=context)
+
+
+def groups_list(request: HttpRequest):
+    context = {
+        "groups": Group.objects.prefetch_related('permissions').all(),
+    }
+    return render(request, 'shopapp/group-list.html', context=context)
+
+
+def products_list(request: HttpRequest):
+    context = {
+        "products": Product.objects.all(),
+    }
+    return render(request, 'shopapp/products-list.html', context=context)
+
+
+def order_list(request: HttpRequest):
+    context = {
+        "orders": Order.objects.select_related("user").prefetch_related("products").all(),
+    }
+    return render(request, 'shopapp/orders-list.html', context=context)
