10912642ddd54152ff64ef6aa2584ed05512aa63
jeen <sejeenn@gmail.com>
2023-03-15 16:22:57 +0300

    10.03.23 homework is done

---
+++ M_05_RequestsAndMiddlewares/mysite/readme.txt
@@ -0,0 +1,2 @@
+This file was downloaded from:
+https://www.usbdev.ru
--- M_05_RequestsAndMiddlewares/mysite/requestdataapp/views.py
+++ M_05_RequestsAndMiddlewares/mysite/requestdataapp/views.py
@@ -1,9 +1,29 @@
+from django.core.files.storage import FileSystemStorage
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
 def process_get_view(request: HttpRequest) -> HttpResponse:
-    context = {
+    a = request.GET.get("a", "")
+    b = request.GET.get("b", "")
+    result = a + b
 
+    context = {
+        "a": a,
+        "b": b,
+        "result": result,
     }
     return render(request, 'requestdataapp/request-query-params.html', context=context)
+
+
+def user_form(request: HttpRequest) -> HttpResponse:
+    return render(request, 'requestdataapp/user-bio-form.html')
+
+
+def handle_file_upload(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST" and request.FILES.get("myfile"):
+        myfile = request.FILES["myfile"]
+        f_system = FileSystemStorage()
+        filename = f_system.save(myfile.name, myfile)
+        print("Сохранен файл с именем:", filename)
+    return render(request, 'requestdataapp/file-upload.html')
