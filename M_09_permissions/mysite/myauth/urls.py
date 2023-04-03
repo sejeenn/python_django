5b25fb9542b2cd9568c822043978f957eea5a7be
jeen <sejeenn@gmail.com>
2023-04-03 16:02:22 +0300

    03.04.23 homework_w (M_09)

--- M_09_permissions/mysite/myauth/urls.py
+++ M_09_permissions/mysite/myauth/urls.py
@@ -1,12 +1,31 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView
+
 
 
 from .views import (
+    set_cookie_view,
+    get_cookie_view,
+    set_session_view,
+    get_session_view,
+    MyLogoutView,
     AboutMeView,
 )
 
 app_name = "myauth"
 
 urlpatterns = [
+path(
+        "login/",
+        LoginView.as_view(
+            template_name="myauth/login.html",
+            redirect_authenticated_user=True,
+        ),
+        name="login"),
+    path("logout/", MyLogoutView.as_view(), name="logout"),
+    path("cookie/get/", get_cookie_view, name="cookie_get"),
+    path("cookie/set/", set_cookie_view, name="cookie_set"),
+    path("session/get/", get_session_view, name="session_get"),
+    path("session/set/", set_session_view, name="session_set"),
     path("about-me/", AboutMeView.as_view(), name="about-me"),
 ]
--- M_09_permissions/mysite/myauth/views.py
+++ M_09_permissions/mysite/myauth/views.py
@@ -1,4 +1,50 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LogoutView
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpRequest, HttpResponse
+from django.urls import reverse, reverse_lazy
+
+
+def login_view(request: HttpRequest) -> HttpResponse:
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect('/admin/')
+
+        return render(request, 'myauth/login.html')
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect("/admin/")
+
+    return render(request, "myauth/login.html", {"error": "Invalid login credentials"})
+
+
+class MyLogoutView(LogoutView):
+    next_page = reverse_lazy("myauth:login")
+
+
+def set_cookie_view(request: HttpRequest) -> HttpResponse:
+    response = HttpResponse("Cookie set")
+    response.set_cookie("fizz", "buzz", max_age=3600)
+    return response
+
+
+def get_cookie_view(request: HttpRequest) -> HttpResponse:
+    value = request.COOKIES.get("fizz", "default value")
+    return HttpResponse(f"Cookie value: {value!r}")
+
+
+def set_session_view(request: HttpRequest) -> HttpResponse:
+    request.session['any_body'] = "какая то хрень"
+    return HttpResponse("Session set!")
+
+
+def get_session_view(request: HttpRequest) -> HttpResponse:
+    value = request.session.get("any_body", "default value")
+    return HttpResponse(f"Session value: {value!r}")
 
 
 class AboutMeView(TemplateView):
