30711c99ed9e380f4828a6a5a9799f8d7f3a1085
jeen <sejeenn@gmail.com>
2023-04-06 13:20:48 +0300

    06.04.23 homework_w (M_09)

--- M_09_permissions/mysite/myauth/models.py
+++ M_09_permissions/mysite/myauth/models.py
@@ -6,4 +6,3 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     agreement_accepted = models.BooleanField(default=False)
-
